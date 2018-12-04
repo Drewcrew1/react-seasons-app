7c9e85b168a702c89e7baf5221bfc9c4a4dca3a2
Drew <drewharper29@gmail.com>
2018-12-04 13:22:44 -0500

    added state with latitude to be rendered

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-
-window.navigator.geolocation.getCurrentPosition((position, err) => {
-    console.log(position);
-    console.log(err);
-
-});
-    return ( <div>Hi There</div>);
 
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        //this is the only time we use direct assignment to this.state
+        this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition((position, err) => {
+            //call set state a function put on component when extended react.component
+            this.setState({ lat: position.coords.latitude });
+
+            console.log(err);
+        });
+    }
+
+    //always define render
+    render() {
+
+
+            return <div>Latitude: {this.state.lat}</div>;
+    }
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
