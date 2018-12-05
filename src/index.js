79ba875e1aad1f265c5845f6e4d53d13ae59d002
Drew <drewharper29@gmail.com>
2018-12-05 11:42:24 -0500

    added seasondisplay, loading screen and season logic

--- src/index.js
+++ src/index.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        //this is the only time we use direct assignment to this.state
-        this.state = { lat: null };
 
-        window.navigator.geolocation.getCurrentPosition((position, err) => {
-            //call set state a function put on component when extended react.component
-            this.setState({ lat: position.coords.latitude });
 
-            console.log(err);
-        });
+    state = { lat: null, errorMessage: ''};
+
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                //call set state a function put on component when extended react.component
+                this.setState({lat: position.coords.latitude});
+            },
+            err => {
+                this.setState({errorMessage: err.message});
+            }
+        );
     }
 
     //always define render
     render() {
 
+            if (this.state.errorMessage && !this.state.lat){
+                return <div> Error: {this.state.errorMessage}</div>
+            }
+            if(!this.state.errorMessage && this.state.lat){
+                return <SeasonDisplay lat={this.state.lat}/>
+            }
+            return <Spinner message="Please accept location request."/>;
 
-            return <div>Latitude: {this.state.lat}</div>;
     }
 };
 
